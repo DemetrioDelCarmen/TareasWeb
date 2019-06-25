a03dc489a7c9aceb421a26b280b916fdf54e4f0c
DemetrioDelCarmen <demeritho95@gmail.com>
2019-06-25 02:01:34 -0500

    arreglar error de que sólo carga un producto y no todos

--- Tareas/puntoDeVenta/js/script.js
+++ Tareas/puntoDeVenta/js/script.js
@@ -5,6 +5,14 @@ let onReady = () =>{
             eliminar()
         } */
     
+
+        var tablaItems = document.createElement("table")
+        tablaItems.setAttribute("id","tablaCompras")
+        tablaItems.className = "table"
+
+
+
+
     let agregarItems = ()=>{
 
             let fila  = document.createElement("tr")
@@ -13,25 +21,28 @@ let onReady = () =>{
             let iCodigo = document.createElement("input")
             iCodigo.type = "text"    
             iCodigo.className = "form-control m-2"
+            iCodigo.setAttribute("id","itemCodigo")
             cCodigo.appendChild(iCodigo)
-
 
             let cNombre = document.createElement("td")
             let iNombre = document.createElement("input")
             iNombre.type = "text"
             iNombre.className = "form-control m-2"
+            iNombre.setAttribute("id","itemNombre")
             cNombre.appendChild(iNombre)
 
 
             let cCantidad = document.createElement("td")
             let iCantidad = document.createElement("input")
             iCantidad.type  = "number"
+            iCantidad.setAttribute("id","itemCantidad")
             iCantidad.className = "form-control m-2"
             cCantidad.appendChild(iCantidad)
 
             let cPrecio = document.createElement("td")
             let iPrecio = document.createElement("input")
             iPrecio.type = "number"
+            iPrecio.setAttribute("id","itemPrecio")
             iPrecio.className = "form-control m-2"
             cPrecio.appendChild(iPrecio)
 
@@ -39,6 +50,7 @@ let onReady = () =>{
             let cTotal = document.createElement("td")
             let iTotal = document.createElement("input")
             iTotal.type = "number"
+            iTotal.setAttribute("id","inputTotal")
             iTotal.className = "form-control m-2"
             cTotal.appendChild(iTotal)
 
@@ -97,11 +109,58 @@ let onReady = () =>{
 
             console.log("hola")
         }
+
+        let  crearNota = () =>{
+                
+                let tablaProductos = document.querySelector("#tableItems")
+                let indexTabla = document.querySelector("#tableItems")
+
+             
+                let areaToReplace = document.querySelector("#infoDiv")
+                let totalPriceArea  = document.querySelector("#totalPriceArea")
+                    let totalResult = 0
+
+              
+                    
+                    
+                    
+                    /* for que crea todo */
+                    let index, table = document.querySelector('table');
+                    for(let i = 1; i < table.rows.length; i++){
+                        
+                        /*     let nombreP = table.rows[i].cells[1] */
+                        
+                        var productName = "";
+
+                        let productoNombre = document.querySelector("#itemNombre").value 
+                        let productoPrecio = document.querySelector("#itemPrecio").value 
+                        let fila = document.createElement("tr")
+                        let columnaNombre  = document.createElement("td")
+                        let textoNombre = document.createTextNode(productoNombre)
+                        columnaNombre.appendChild(textoNombre)                    
+    
+                        let columnaPrecio = document.createElement("td")
+                        let valorPrecio = document.createTextNode(productoPrecio)
+                        columnaPrecio.appendChild(valorPrecio)
+    
+                        fila.appendChild(columnaNombre)
+                        fila.appendChild(columnaPrecio)
+    
+                        tablaItems.appendChild(fila)     
+                        
+                        console.log(table.rows[i].cells[1].value)
+                    };
+                    /* end for que crea todo */
+
+
+                    areaToReplace.appendChild(tablaItems) 
+                
+        }
             
         
 
    
-    
+    document.querySelector("#crearNota").addEventListener("click",crearNota)
     document.querySelector("#agregar").addEventListener("click",agregarItems)
     document.querySelector("#eliminar").addEventListener("click",deleteItems) 
       
